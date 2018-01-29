d6e0dc064b346b80d37611c4a187a9f64a1cb580
maya <maya.schmidt@baxter-academy.org>
2018-01-29 09:29:58 -0500

    setting up imports for OAuth w/ google forms

---
+++ dict.py
@@ -0,0 +1,12 @@
+#compatability package recommended in google api thing
+from __future__ 
+import print_function
+#lets me open spreadsheet by url :D
+import gspread
+#using Storage for json authorization instead of SignedJwtAssertionCredentials b/c the internet was angry... discontinued
+from oauth2client.file.Storage(credential_holder)
+#pandas- lib for data analysis
+import pandas as pd
+#hold secretysecret file
+import json
+
