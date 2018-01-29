37e8869f61e5ee2bddef6adc682c72ce79e221a5
maya <maya.schmidt@baxter-academy.org>
2018-01-29 13:23:47 -0500

    being less terrible

--- dict.py
+++ dict.py
@@ -1,11 +1,20 @@
 #compatability package recommended in google api thing
+import credential_holder
+import oauth2client
 from __future__ 
 import print_function
 #lets me open spreadsheet by url :D
-import gspread
-#using Storage for json authorization instead of SignedJwtAssertionCredentials b/c the internet was angry... discontinued
+import gspread from oauth2client
 #pandas- lib for data analysis
 import pandas as pd
 #hold secretysecret file
 import json
 
+
+#using Storage for json authorization instead of SignedJwtAssertionCredentials b/c the internet was angry; it's been discontinued
+credinstance = oauth2client.file.Storage()
+class CredentialExchange:
+#put creds in holder
+
+def putcredentials(credinstance):
+    storecreds.locked_put(json)
