bb0d8b756f7009aceff8f7840b194de442c55ae0
maya <maya.schmidt@baxter-academy.org>
2018-01-29 13:58:51 -0500

    butts

--- dict.py
+++ dict.py
@@ -1,6 +1,6 @@
 #compatability package recommended in google api thing
 import credential_holder
-import oauth2client
+import oauth2client.file.Storage
 from __future__ 
 import print_function
 #lets me open spreadsheet by url :D
@@ -12,9 +12,11 @@ import json
 
 
 #using Storage for json authorization instead of SignedJwtAssertionCredentials b/c the internet was angry; it's been discontinued
-credinstance = oauth2client.file.Storage()
 class CredentialExchange:
 #put creds in holder
 
-def putcredentials(credinstance):
-    storecreds.locked_put(json)
+store = oauth2client.file.Storage
+
+def putcredentials(credential_holder):
+    store.locked_put(credential_holder)
+    
