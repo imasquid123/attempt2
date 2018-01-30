cdeba5ea038d6f89e86be0336b3765c5515b6fcb
maya <maya.schmidt@baxter-academy.org>
2018-01-30 09:40:22 -0500

    bepis is dead

--- dict.py
+++ dict.py
@@ -1,21 +1,17 @@
 #compatability package recommended in google api thing
 import credential_holder
 import oauth2client.file.Storage
+
 from __future__ 
 import print_function
 #lets me open spreadsheet by url :D
-import gspread from oauth2client
+import gspread
 #pandas- lib for data analysis
 import pandas as pd
 #hold secretysecret file
 import json
 
 store = oauth2client.file.Storage()
-
-
-class bepis(oauth2client):
-    store = bepis.file.Storage()
-
 
 #using Storage for json authorization instead of SignedJwtAssertionCredentials b/c the internet was angry; it's been discontinued
 class CredentialExchange:
